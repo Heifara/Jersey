b21f0dcd6d633f030c70b9755d82ac6945d91380
Heifara MATAPO <heifaramatapo@gmail.com>
2015-06-02 14:53:14 -1000

    Using jaxb to read data from url and transform it into object

--- webapp/src/test/java/com/maohi/software/webapp/TodoResourceTest.java
+++ webapp/src/test/java/com/maohi/software/webapp/TodoResourceTest.java
@@ -1,10 +1,16 @@
 package com.maohi.software.webapp;
 
+import java.net.MalformedURLException;
 import java.net.URI;
+import java.net.URISyntaxException;
 
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.UriBuilder;
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Unmarshaller;
 
+import com.maohi.software.webapp.jaxb2.Todo;
 import com.sun.jersey.api.client.Client;
 import com.sun.jersey.api.client.WebResource;
 import com.sun.jersey.api.client.config.ClientConfig;
@@ -26,6 +32,21 @@ public class TodoResourceTest {
 		System.out.println(service.path("webresources").path("todo").accept(MediaType.APPLICATION_JSON).get(String.class));
 		// Get JSON for application
 		System.out.println(service.path("webresources").path("todo").accept(MediaType.APPLICATION_XML).get(String.class));
+
+		try {
+			JAXBContext jaxbContext = JAXBContext.newInstance(Todo.class);
+			Unmarshaller jaxbUnmarshaller = jaxbContext.createUnmarshaller();
+			Todo iTodo = (Todo) jaxbUnmarshaller.unmarshal(new URI("http://localhost:8080/webapp/webresources/todo").toURL());
+			System.out.println(iTodo);
+
+		} catch (JAXBException e) {
+			e.printStackTrace();
+		} catch (MalformedURLException e) {
+			e.printStackTrace();
+		} catch (URISyntaxException e) {
+			e.printStackTrace();
+		}
+
 	}
 
 	private static URI getBaseURI() {
